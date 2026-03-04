ee9749e776dfcc5fc66111ac98a415bcd5fcc88d
Amresh Yadav <ayadav.1918@gmail.com>
2026-03-04 14:14:33 +0530

    initialized repo

---
+++ src/store/use-ui-store.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+
+type UIState = {
+  isSidebarOpen: boolean;
+  setSidebarOpen: (isOpen: boolean) => void;
+  toggleSidebar: () => void;
+};
+
+export const useUIStore = create<UIState>((set) => ({
+  isSidebarOpen: false,
+  setSidebarOpen: (isOpen) => set({ isSidebarOpen: isOpen }),
+  toggleSidebar: () =>
+    set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
+}));
